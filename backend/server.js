a5f9c98afdc89382fb0b7e21610812835663f6cf
Santhosh-k-dot <ramkrishna90350@gmail.com>
2025-01-20 11:27:03 +0530

    Update .gitignore and modify server.js for better error handling

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,32 @@
+// backend/server.js
+
+
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const mongoose = require('mongoose');
+
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// parse options
+app.use(express.json());
+app.use(cors())
+
+
+
+async function main() {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    app.get('/', (req, res) => {
+        res.send('Server is Running...!')
+    })
+
+}
+
+
+main().then(() => console.log("Connected to MongoDB Sucessfully")).catch(err => console.log(err));
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
